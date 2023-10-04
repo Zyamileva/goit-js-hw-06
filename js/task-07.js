d63a7b87b117b757586b61a88ab6a75f545a7542
Lena <elenazyamileva@gmail.com>
2023-10-05 00:25:29 +0300

    Update Task7

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,14 @@
 "use strict";
 let inputForm = document.getElementById("font-size-control");
 let textForm = document.getElementById("text");
-inputForm.oninput = function() {
-    textForm.style.fontSize = inputForm.value + "px";
-};
 
+window.addEventListener("load", loadStartValue);
+inputForm.addEventListener("input", inputValue);
+
+function loadStartValue(evt) {
+  textForm.style.fontSize = inputForm.value + "px";
+}
+
+function inputValue(evt) {
+  textForm.style.fontSize = evt.currentTarget.value + "px";
+}
